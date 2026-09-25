5f4204f5e56fbfdf0aefd1a31a03db57dab6bc8e
ThanhNguyn <253024274+ThanhNguyn@users.noreply.github.com>
2026-09-25 05:30:01 -0400

    Add print function to display N-Queens solution

--- xephau.cpp
+++ xephau.cpp
@@ -5,9 +5,24 @@ using namespace std;
 vector<bool> doc;
 vector<bool> cheo1;
 vector<bool> cheo2;
+vector<int> x;
 
 int n;
 
+void print(){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(doc[j] && cheo1[i + j] && cheo2[i - j + n]){
+                cout << "Q ";
+            }
+            else{
+                cout << ". ";
+            }
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
 // i is the row, j is the column
 void Attempt(int i){
     for(int j = 1;j <= n; j++){
@@ -15,7 +30,9 @@ void Attempt(int i){
             doc[j] = true;
             cheo1[i + j] = true;
             cheo2[i - j + n] = true;
+            x[i] = j;
             if(i == n){
+                print();
             }
             else{
                 Attempt(i + 1);
